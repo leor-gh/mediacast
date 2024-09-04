a8678ba03bce51ce4950d44c936b1a7ba9386aa8
leor-gh <59747045+leor-gh@users.noreply.github.com>
2024-09-04 13:45:32 -0700

    Modify deploy.sh to use git worktree to manage the gh pages branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env sh
 
+branch=new-gh-pages
+
 # abort on errors
 set -e
+
+# set up git worktree that maps to the gh pages branch with dist as the root
+worktree=$(git worktree list | awk "\$3 == \"[$branch]\" { print \$0 }")
+[ -z "$worktree" ] && {
+    rm -fr dist
+    git worktree add dist $branch
+}
 
 # build
 npm run build
 
+# npm build deletes the dist directory.  need to restore the worktree config
+echo gitdir: ../.git/worktrees/dist > dist/.git
+
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
 git add -A
-git commit -m 'deploy'
-
-git push -f git@github.com:alfg/mediacast.git master:gh-pages
-
-cd -
